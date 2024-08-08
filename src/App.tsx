bc292226aef9a19308c8568cd092321401ea5639
Kate Rybiy <kporhun@gmail.com>
2024-08-08 12:45:43 -0700

    simple form usingFormik and yup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import LoginForm from './components/LoginForm'
+
+function App() {
+  
+
+  return (
+    <div className='container'>
+      <LoginForm />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,54 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+
+const LoginForm = () => {
+  const validationSchema = Yup.object({
+    login: Yup.string()
+      .required("Login is required")
+      .min(3, "Login must be at least 3 characters")
+      .max(15, "Login must be at most 15 characters"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters")
+      .max(20, "Password must be at most 20 characters"),
+  });
+
+  const handleSubmit = (values: { login: string; password: string }) => {
+    console.log("Form submitted", values);
+  };
+
+  return (
+    <Formik
+      initialValues={{ login: "", password: "" }}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
+    >
+      <Form>
+        <div className="form-group">
+          <label htmlFor="login">Login</label>
+          <Field id="login" name="login" type="text" className="form-control" />
+          <ErrorMessage name="login" component="div" className="form-error" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <Field
+            id="password"
+            name="password"
+            type="password"
+            className="form-control"
+          />
+          <ErrorMessage
+            name="password"
+            component="div"
+            className="form-error"
+          />
+        </div>
+        <button type="submit" className="form-submit-btn">
+          Submit
+        </button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default LoginForm;
